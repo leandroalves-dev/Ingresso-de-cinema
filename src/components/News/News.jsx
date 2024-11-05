5811eaebfd51134f51072cf41442a7d101610556
leandroeffgen8 <leandroeffgen8@gmail.com>
2024-11-05 08:45:15 -0300

    alteração no arquivo news

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -17,7 +17,7 @@ const News = () => {
                     
                     {news.movieNews.map((news, index) => (
                         <div className="news" key={index}>
-                            <img src={`../${news.image}`} alt={news.title} title={news.title}  />
+                            <img src={news.image} alt={news.title} title={news.title}  />
                             <Link to={`/details-news/${news.id}`}>{news.title}</Link>
                             <span className='data'>{news.data}</span>
                         </div>
